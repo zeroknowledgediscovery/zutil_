06ce7bb1e5d4cd540d9483466c423350157e356d
Yi Huang <yhuang10@midway2-login2.rcc.local>
2020-04-14 16:26:43 -0500

    with or without user defined PFSA base for run_lsmash_classfication

--- run_lsmash_classfication.sh
+++ run_lsmash_classfication.sh
@@ -28,21 +28,30 @@ awk -F"\t" '{print $1}' $2 > $test_label_file
 awk -F"\t" '{for(i=2;i<=NF;i++){ printf("%s",( (i>2) ? " " : "" ) $i) } ; print ;}' $1 > $train_seq_file
 awk -F"\t" '{for(i=2;i<=NF;i++){ printf("%s",( (i>2) ? " " : "" ) $i) } ; print ;}' $1 > $test_seq_file
 
-cmd="./lsmash_classification -f $train_seq_file -g $test_seq_file -l $train_label_file -k $test_label_file -c $train_coord_file -d $test_coord_file -u 1 -P 0 -T continuous -F baseChoice_cfgfiles/PFSA_0.cfg baseChoice_cfgfiles/PFSA_1.cfg baseChoice_cfgfiles/PFSA_2.cfg baseChoice_cfgfiles/PFSA_3.cfg baseChoice_cfgfiles/PFSA_4.cfg"
+# cmd="./lsmash_classification -f $train_seq_file -g $test_seq_file -l $train_label_file -k $test_label_file -c $train_coord_file -d $test_coord_file -u 1 -P 0 -T continuous -F baseChoice_cfgfiles/PFSA_0.cfg baseChoice_cfgfiles/PFSA_1.cfg baseChoice_cfgfiles/PFSA_2.cfg baseChoice_cfgfiles/PFSA_3.cfg baseChoice_cfgfiles/PFSA_4.cfg"
 
+cmd="./lsmash_classification -f $train_seq_file -g $test_seq_file -l $train_label_file -k $test_label_file -c $train_coord_file -d $test_coord_file -u 1 -P 0 -T continuous"
+num_arg=$#
+if [[ $num_arg == 3 ]]
+then
+	if [[ -f $3/PFSAfiles ]]
+	then
+		rm $3/PFSAfiles
+	fi
+	
+	for file in `ls $3`
+	do
+		echo $3/${file} >> $3/PFSAfiles
+	done
+	sed -i ':a;N;$!ba;s/\n/ /g' $3/PFSAfiles
+	PFSAfiles=`cat $3/PFSAfiles`
+	cmd="${cmd} -F $PFSAfiles"
+fi
+echo $cmd
+eval $cmd
 
-if [[ -f $3/PFSAfiles ]]
+rm $train_seq_file $test_seq_file $train_label_file $test_label_file 
+if [[ $num_arg == 3 ]]
 then
 	rm $3/PFSAfiles
 fi
-
-for file in `ls $3`
-do
-	echo $3/${file} >> $3/PFSAfiles
-done
-sed -i ':a;N;$!ba;s/\n/ /g' $3/PFSAfiles
-PFSAfiles=`cat $3/PFSAfiles`
-cmd="./lsmash_classification -f $train_seq_file -g $test_seq_file -l $train_label_file -k $test_label_file -c $train_coord_file -d $test_coord_file -u 1 -P 0 -T continuous -F $PFSAfiles"
-echo $cmd
-eval $cmd
-rm $train_seq_file $test_seq_file $train_label_file $test_label_file $3/PFSAfiles
